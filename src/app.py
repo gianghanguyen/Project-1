e738cb13f3a0e00c681ce286f881db693010bc0f
gianghanguyen <giangnguyen20032010@gmail.com>
2023-12-31 01:40:08 +0700

    fix bug

--- src/app.py
+++ src/app.py
@@ -15,7 +15,7 @@ def convertOutput(nested_list):
     return [element for sublist in nested_list for element in sublist]
 
 @app.route('/astar-solver', methods=['GET'])
-def astar():
+def solve1():
     raw_input = request.args.get('pntdata').split(',')
     puzzle = Puzzle(convertInput(raw_input))
     solvable = puzzle.isSolvable()
@@ -23,15 +23,15 @@ def astar():
     if solvable:
         tic=time.perf_counter()
         s = Solver(puzzle)
-        toc = time.perf_counter()
         path = s.astar()
-        print("Total amount of time in search: {:.9f} second(s)".format(toc - tic))
+        toc = time.perf_counter()
+        print("Total amount of time in astar-search: {:.9f} second(s)".format(toc - tic))
         return ([convertOutput(node.puzzle.board) for node in path])
     else:
         return "Unable to solve", 400
 
 @app.route('/dfs-solver', methods=['GET'])
-def dfs():
+def solve2():
     raw_input = request.args.get('pntdata').split(',')
     puzzle = Puzzle(convertInput(raw_input))
     solvable = puzzle.isSolvable()
@@ -39,20 +39,21 @@ def dfs():
     if solvable:
         tic=time.perf_counter()
         s = Solver(puzzle)
-        toc = time.perf_counter()
-        depthLimit = int(request.args.get('depth_limit', default=100))
+        depthLimit = int(request.args.get('depth_limit', default=0))
         nodeLimit = int(request.args.get('node_limit', default=1000000))
-        path = s.dfs(depthLimit, nodeLimit)
+        dfs_path = s.dfs(depthLimit, nodeLimit)
+        toc = time.perf_counter()
         print("Total amount of time in dfs-search: {:.9f} second(s)".format(toc - tic))
-        if path != None:
-            return ([convertOutput(node.puzzle.board) for node in path])
+        if dfs_path != None:
+            return ([convertOutput(node.puzzle.board) for node in dfs_path])
         else: 
             return "Cant find solution with depth " + str(depthLimit)
     else:
         return "Unable to solve", 400
 
+
 @app.route('/bfs-solver', methods=['GET'])
-def bfs():
+def solve3():
     raw_input = request.args.get('pntdata').split(',')
     puzzle = Puzzle(convertInput(raw_input))
     solvable = puzzle.isSolvable()
@@ -60,17 +61,18 @@ def bfs():
     if solvable:
         tic=time.perf_counter()
         s = Solver(puzzle)
-        depthLimit = int(request.args.get('depth_limit', default=100))
+        depthLimit = int(request.args.get('depth_limit', default=0))
         nodeLimit = int(request.args.get('node_limit', default=1000000))
-        path = s.bfs(depthLimit, nodeLimit)
+        bfs_path = s.bfs(depthLimit, nodeLimit)
         toc = time.perf_counter()
-        print("Total amount of time in search: {:.9f} second(s)".format(toc - tic))
+        print("Total amount of time in bfs-search: {:.9f} second(s)".format(toc - tic))
 
-        if path != None:
-            return ([convertOutput(node.puzzle.board) for node in path])
+        if bfs_path != None:
+            return ([convertOutput(node.puzzle.board) for node in bfs_path])
 
         else: 
             return "Cant find solution with depth " + str(depthLimit)
+            
     else:
         return "Unable to solve", 400
 
